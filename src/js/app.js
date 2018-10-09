2cb977ff0aaeaa1ed2e2b76dfee4da3127422e79
Jeremias Castañeda Arreola <jeremias.janai@outlook.com>
2018-10-08 21:48:39 -0500

    tipos de datos y comentarios en javascrip

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+// Comentarios en Javascript 
+
+// Comentario Lineal
+
+/*
+Multiples líneas
+*/ 
+
+/**
+ * Multiples
+ * Lineas
+ * Con asteriscos
+ * Tipos de datos:
+ */
+
+// Cinco tipos de datos
+
+// Numero
+var numero = 10;
+
+// String
+var String = "Esto es un string";
+var String3 = `Esto es un string moderno`;
+
+// Boolean "El algebra booleana es verdadero o falso"
+var Boolean1 = true;
+var Boolean2 = false;
+
+// Null
+var vacio = null;
+
+// Udefined
+var indefinido = undefined;
+
+
